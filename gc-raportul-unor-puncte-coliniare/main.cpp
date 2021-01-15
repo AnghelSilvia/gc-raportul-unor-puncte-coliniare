741176d9958db1657504657c9dd722f6dc98b7fa
Silvia Anghel <silvia_anghel98@yahoo.com>
2021-01-15 22:46:16 +0200

    Added computeRatio function, two similar implementations

--- gc-raportul-unor-puncte-coliniare/main.cpp
+++ gc-raportul-unor-puncte-coliniare/main.cpp
@@ -39,20 +39,57 @@ void readInput(ifstream& inputFile, vector<Point>& points)
         }
         points.push_back(tempPoint);
     }
-
-
 }
 
 void printPoints(const vector<Point>& points)
 {
     int i, j;
     cout << "Punctele sunt: \n";
-    for (i = 0; i < points.size(); ++i) {
+    for (i = 0; i < points.size(); ++i)
+    {
         cout << points[i].name << "(";
         for (j = 0; j < points[i].coordinates.size() - 1; ++j)
             cout << points[i].coordinates[j] << ", ";
-        cout << points[i].coordinates[j] << ")\n";
+        cout << points[i].coordinates[j] << ")";
+        cout << "\n";
     }
+    cout << "\n";
+}
+
+float computeRatioOptimized(const Point& M, const Point& N, const Point &P)
+{
+    float MN, NP, r;
+    //MN = N - M; NP = P - N
+    MN = N.coordinates[0] - M.coordinates[0];
+    NP = P.coordinates[0] - N.coordinates[0];
+    r = MN / NP;
+    return r;
+}
+
+//needed for printing MN = r * NP
+//otherwise use optimized version
+float computeRatio(const Point& M, const Point& N, const Point& P)
+{
+    int i;
+    float r;
+    vector<float> MN, NP;
+    for (i = 0; i < M.coordinates.size(); ++i)
+        MN.push_back(N.coordinates[i] - M.coordinates[i]);
+    for (i = 0; i < M.coordinates.size(); ++i)
+        NP.push_back(P.coordinates[i] - N.coordinates[i]);
+    r = MN[0] / NP[0];
+
+    //printing
+    cout << M.name << N.name <<"(";
+    for (i = 0; i < MN.size() - 1; ++i)
+        cout << MN[i] << ", ";
+    cout << MN[i] << ") = " << r << " * ";
+    cout << N.name << P.name << "(";
+    for (i = 0; i < NP.size() - 1; ++i)
+        cout << NP[i] << ", ";
+    cout << NP[i] << ")\n";
+
+    return r;
 }
 
 int main()
